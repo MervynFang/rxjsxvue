a24eb56b12fa46a2e79e5a6a07fe22e30febe895
FangMervyn <mervynfang@gmail.com>
2016-10-03 01:09:13 +0800

    add vue-rx to project and fix bug pull request

--- src/views/1.vue
+++ src/views/1.vue
@@ -1,16 +1,30 @@
 <template lang="html">
   <div class="one">
     {{a}}
+    <ul v-for="val in b">
+      <li>{{val}}</li>
+    </ul>
+    <button type="button" name="button" class="btn">click log</button>
     <router-link to="/hi">to 2</router-link>
   </div>
 </template>
 
 <script>
+import Rx from 'rxjs/Rx'
+
+const aaa = Rx.Observable.from([10, 20, 30]).map(val => val)
+
 export default {
   data () {
     return {
-      a: 1
+      a: 1,
+      b: aaa
     }
+  },
+  mounted () {
+    Rx.Observable.fromEvent(document.querySelector('.btn'), 'click').subscribe(() => {
+      console.log(123)
+    })
   }
 }
 </script>
