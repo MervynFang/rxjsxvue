3f21145d9935ebc54e2c616967b12ef13f1b7fc1
FangMervyn <mervynfang@gmail.com>
2016-10-03 01:40:11 +0800

    add jsx writing style

--- src/views/2.vue
+++ src/views/2.vue
@@ -1,9 +1,9 @@
-<template lang="html">
+<!-- <template lang="html">
   <div class="two">
     {{b}}
     <router-link to="/hello">to 1</router-link>
   </div>
-</template>
+</template> -->
 
 <script>
 export default {
@@ -11,6 +11,14 @@ export default {
     return {
       b: 2
     }
+  },
+  render (h) {
+    return (
+      <div class={'two'}>
+        {this.b}
+        <router-link to='/hello' class={'haha'}>to 1</router-link>
+      </div>
+    )
   }
 }
 </script>
@@ -18,5 +26,8 @@ export default {
 <style lang="css">
 .two {
   color: #aed123;
+}
+.haha {
+  display: block;
 }
 </style>
