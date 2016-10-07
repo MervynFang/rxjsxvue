0dae7c1408926a97be67b4ccd559b2ec3e830da9
FangMervyn <mervynfang@gmail.com>
2016-10-07 15:50:20 +0800

    add jsx demo

--- src/views/2.vue
+++ src/views/2.vue
@@ -9,13 +9,23 @@
 export default {
   data () {
     return {
-      b: 'this is a vue 2.0 jsx demo page'
+      b: 'this is a vue 2.0 jsx demo page',
+      d: [10, 20, 30]
     }
   },
   render (h) {
+    const coljsx = []
+    this.d.map((val, index) => {
+      coljsx.push(<li>{val}</li>)
+    })
+    
     return (
       <div class={'two'}>
         {this.b}
+        <ul>
+          {/* we dont need v-for directive here with jsx writing style */}
+          {coljsx}
+        </ul>
         <router-link to='/hello' class={'haha'}>to 1</router-link>
       </div>
     )
