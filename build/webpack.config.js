517a243b1ac929d97c15a0c8552003b721716034
FangMervyn <mervynfang@gmail.com>
2016-11-26 23:15:03 +0800

    extract chunk to manifest

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -78,7 +78,7 @@ const baseConfig = {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
+      name: ['vendor', 'manifest']
     })
   ]
 }
