9deabf5950074c9f68f01e6361a08a1928f89571
FangMervyn <mervynfang@gmail.com>
2016-11-27 15:36:14 +0800

    change app name

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <div class="rerer a">
+      vue2 webpack2
+    </div>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      a: 'ha123'
+    }
+  },
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+@import "./assets/css/reset.css";
+
+.rerer {
+  color: #ca699d;
+}
+</style>
